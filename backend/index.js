8592b93e1367a1a8e8c0681230e97c2130f7161b
jaimin1503 <123434935+jaimin1503@users.noreply.github.com>
2024-07-18 16:13:39 +0530

    backend init

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import connect from "./utils/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import cloudinaryConnect from "./utils/cloudinary.js";
+
+connect();
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["https://do-remote.vercel.app", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp",
+  })
+);
+cloudinaryConnect();
+
+// app.use("/user", userRoutes);
+// app.use("/job", jobRoutes);
+// app.use("/proposal", proposalRoutes);
+// app.use("/profile", profileRoutes);
+// app.use("/client", clientRoutes);
+
+const port = process.env.PORT || 5555;
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}`);
+});
